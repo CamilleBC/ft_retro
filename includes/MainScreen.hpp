2327878013437f735980e14ecb8d7f8394f95610
Camille Baillat <CamilleBC@users.noreply.github.com>
2019-05-24 22:34:49 +0200

    Initial commit

---
+++ includes/MainScreen.hpp
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   MainScreen.hpp                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: cbaillat <cbaillat@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/05/24 22:56:31 by cbaillat          #+#    #+#             */
+/*   Updated: 2019/05/24 23:25:00 by cbaillat         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MAINSCREEN_HPP
+#define MAINSCREEN_HPP
+
+class MainScreen {
+  private:
+  public:
+    MainScreen();
+    ~MainScreen();
+};
+
+#endif /* MAINSCREEN_HPP */
---
+++ srcs/main.cpp
@@ -0,0 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: cbaillat <cbaillat@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/05/24 22:57:40 by cbaillat          #+#    #+#             */
+/*   Updated: 2019/05/24 23:25:00 by cbaillat         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <cstdlib> // exit codes
+#include <iostream>
+
+int main(void) {
+    std::cout << "ft_retro ftw!!" << std::endl;
+
+    return EXIT_SUCCESS;
+}
